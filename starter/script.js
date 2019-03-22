33b22a6f0d3b2db6b8ddc1181a9e59241f78fff5
MurrTuck <murrtuck@gmail.com>
2019-03-21 23:21:50 -0600

    Variables and Data Types

---
+++ starter/script.js
@@ -0,0 +1,15 @@
+var firstName = 'Murr';
+console.log(firstName)
+
+var lastName = 'Tuck';
+var age = 33;
+console.log(lastName, age)
+
+var fullAge = true;
+console.log(fullAge)
+
+var job;
+console.log(job)
+
+job = 'Teacher'
+console.log(job);
