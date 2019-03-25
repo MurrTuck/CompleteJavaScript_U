c55e6f2bfda18388a7294a8fd2c3d8bd0197f012
MurrTuck <murrtuck@gmail.com>
2019-03-24 23:13:19 -0600

    Type Coercion & Variable Mutation

--- starter/script.js
+++ starter/script.js
@@ -1,15 +1,36 @@
-var firstName = 'Murr';
-console.log(firstName)
+// var firstName = 'Murr';
+// console.log(firstName)
 
-var lastName = 'Tuck';
-var age = 33;
-console.log(lastName, age)
+// var lastName = 'Tuck';
+// var age = 33;
+// console.log(lastName, age)
 
-var fullAge = true;
-console.log(fullAge)
+// var fullAge = true;
+// console.log(fullAge)
 
-var job;
-console.log(job)
+// var job;
+// console.log(job)
 
-job = 'Teacher'
-console.log(job);
+// job = 'Teacher'
+// console.log(job);
+
+var firstName = 'Murr'
+var age = '33'
+
+console.log(firstName + ' ' + age)
+
+// You can call upon many variables just like the example below:
+var job, isMarried;
+job = 'teacher';
+isMarried = true;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'thirty three';
+job = 'coder'
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName)
