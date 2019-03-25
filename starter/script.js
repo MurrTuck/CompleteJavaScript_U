d6c12925edd655ea71cac5c36fd8eaa5e09ca8c4
MurrTuck <murrtuck@gmail.com>
2019-03-25 00:00:12 -0600

    Math Operators, Logitcal Operators, typeof Operator

--- starter/script.js
+++ starter/script.js
@@ -14,23 +14,52 @@
 // job = 'Teacher'
 // console.log(job);
 
-var firstName = 'Murr'
-var age = '33'
+// var firstName = 'Murr'
+// var age = '33'
 
-console.log(firstName + ' ' + age)
+// console.log(firstName + ' ' + age)
 
-// You can call upon many variables just like the example below:
-var job, isMarried;
-job = 'teacher';
-isMarried = true;
+// // You can call upon many variables just like the example below:
+// var job, isMarried;
+// job = 'teacher';
+// isMarried = true;
 
-console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-// Variable mutation
-age = 'thirty three';
-job = 'coder'
+// // Variable mutation
+// age = 'thirty three';
+// job = 'coder'
 
-alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+// alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
-var lastName = prompt('What is his last name?');
-console.log(firstName + ' ' + lastName)
+// var lastName = prompt('What is his last name?');
+// console.log(firstName + ' ' + lastName)
+
+// **************************************
+// Basic Oporators
+
+var year, yearMurr, yearKels;
+year = 2019
+ageMurr = 34;
+ageKels = 33
+
+// Math operators
+yearMurr = year - ageMurr;
+yearKels = year - ageKels;
+
+console.log(yearMurr)
+
+console.log(year + 5)
+console.log(year * 5)
+console.log(year / 5)
+
+// Logical operators
+var whosOlder = ageMurr < ageKels;
+console.log(whosOlder)
+
+// typof Operator
+console.log(typeof whosOlder)
+console.log(typeof ageMurr);
+console.log(typeof 'Murr is older than Kels');
+var x;
+console.log(typeof x);
